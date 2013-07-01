7d4f9191d814a466407739ea1c6049d42db1b4e2
anardy <macknardy@gmail.com>
2013-06-30 21:58:00 -0300

    Arquivos deletados

--- application/models/Inscricoes.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Inscricao extends Eloquent {
-	public static $timestamps = false;
-
-}
--- public/js/script.inscricao.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-	$("#cpf").inputmask("mask", {"mask": "999.999.999-99"});
-	$("#telefone").inputmask("mask", {"mask": "(99) 9999-9999"});
-});
